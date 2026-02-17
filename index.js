f370553db084beec71e319e0a30346e5cd75e58d
Jupri <261552404+juspare25@users.noreply.github.com>
2026-02-17 16:13:58 +0700

    Change "Guest" header to "Away"

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let homeScore = 0
-let guestScore = 0
+let awayScore = 0
 let scoreEl1 = document.getElementById("score-el1");
 let scoreEl2 = document.getElementById("score-el2");
 
@@ -18,17 +18,17 @@ function add3Home() {
     scoreEl1.textContent = homeScore;
 }
 
-function add1Guest() {
-    guestScore += 1;
-    scoreEl2.textContent = guestScore;
+function add1Away() {
+    awayScore += 1;
+    scoreEl2.textContent = awayScore;
 }
 
-function add2Guest() {
-    guestScore += 2;
-    scoreEl2.textContent = guestScore;
+function add2Away() {
+    awayScore += 2;
+    scoreEl2.textContent = awayScore;
 }
 
-function add3Guest() {
-    guestScore += 3;
-    scoreEl2.textContent = guestScore;
+function add3Away() {
+    awayScore += 3;
+    scoreEl2.textContent = awayScore;
 }
